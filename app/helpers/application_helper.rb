d8b0dc2756691a8ea38503aa072de916b2aaf0a1
cavery8989 <c.avery1989@gmail.com>
2016-02-04 12:51:05 +0000

    Added before actions

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,12 @@
 module ApplicationHelper
   
-  def gravatar_for (chef, options = {size: 80})
+  def gravatar_for (chef, options = {size: 80}, cssclass = "")
     gravatar_id = Digest::MD5::hexdigest(chef.email.downcase)
     size = options[:size]
+    custom_class = "gravatar " + cssclass
+ 
+    
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
-    image_tag(gravatar_url, alt: chef.name, class: "gravatar")
+    image_tag(gravatar_url, alt: chef.name, class: custom_class)
   end
 end
