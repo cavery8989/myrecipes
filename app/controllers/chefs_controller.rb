ebabfaf0778a172c806fe97ae458e2a55fd385e6
cavery8989 <c.avery1989@gmail.com>
2016-02-04 14:59:07 +0000

    Added navition tabs tothe tops of index pages.

--- app/controllers/chefs_controller.rb
+++ app/controllers/chefs_controller.rb
@@ -4,7 +4,7 @@ class ChefsController < ApplicationController
   
   
   def index
-    @chefs = Chef.paginate(page: params[:page], per_page: 2)
+    @chefs = Chef.paginate(page: params[:page], per_page: 5)
   end
   
   def new
